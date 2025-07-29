6b9794c212b800c6c633c382b7ec4d852099a4f5
Matias <lopezaedo.matias@gmail.com>
2025-07-29 03:20:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -52,7 +52,8 @@ async function fetchStarlinkData() {
 fetchStarlinkData();
 schedule.scheduleJob('*/30 * * * *', fetchStarlinkData);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor corriendo en http://0.0.0.0:${PORT}`);
 });
